3afe154fbec144079b2885b25186cc9f61905bd3
Evelyn <evelynmantovani.dv@gmail.com>
2025-08-27 22:14:49 -0300

    projeto api_pessoas

---
+++ API_PESSOAS/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+// constate pessoas como uma lista
+
+//rota para cadastro de pessoas
+//rota para consultar pessoas
+//rota consulta por nome
+//rota para deletar pessoa
+
+//usar o insominia
+
+
+app.listen(8080, ()=>{
+    console.log("Servidor rodando na porta 8080");
+});
